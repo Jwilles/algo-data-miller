36a5e73084cfad1eb6dc833b986a526ed21350ff
John <johnwilles@gmail.com>
2017-04-11 23:44:11 -0400

    Finish Median quicksort

--- divide-conquer/week3/quickSort.py
+++ divide-conquer/week3/quickSort.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 intfile = open('QuickSortInput.txt', 'r')
 intArr = intfile.read().split('\r\n')
 intArr.pop()
@@ -8,7 +10,7 @@ testArr = [1,3,5,2,4,6]
 testArr2 = [6,5,4,3,2,1]
 
 def choosePivot(arr):
-  return arr[0]
+  return [arr[0], arr]
 
 def chooseLastPivot(arr):
   n = len(arr)
@@ -16,7 +18,20 @@ def chooseLastPivot(arr):
   arr = swap(arr, 0, n-1)
   return [pivot, arr]
 
-def chooseMedianPivot:
+def chooseMedianPivot(arr):
+  n = len(arr)
+  p0 = arr[0]
+  pm = arr[n/2]
+  pn = arr[n-1]
+  pivot = np.median(np.array([p0, pm, pn]))
+  if pivot == p0:
+    return [arr[0], arr]
+  elif pivot == pm:
+    arr = swap(arr, 0, n/2)
+    return [pivot, arr]
+  elif pivot == pn:
+    arr = swap(arr, 0, n-1)
+    return [pivot, arr]
 
   
 def swap (arr, i, j):
@@ -31,8 +46,10 @@ def quickSort(arr):
 
   if n < 2:
     return [0, arr]
-  
-  p = chooseLastPivot(arr)
+ 
+#  p = choosePivot(arr)
+#  p = chooseLastPivot(arr)
+  p = chooseMedianPivot(arr)
   pivot = p[0]
   arr = p[1]
   i = 1
@@ -47,5 +64,5 @@ def quickSort(arr):
   return [comparisonTotal, sortedArr]
     
  
-print quickSort(intArr) 
+print quickSort(intArr)[0] 
 
