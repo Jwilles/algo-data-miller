20627014f050df63d362bdb8b04c59eae7b3b3c0
John <johnwilles@gmail.com>
2017-06-24 16:05:03 -0400

    Add minCut and graph-search

--- divide-conquer/week4/minCut.py
+++ divide-conquer/week4/minCut.py
@@ -6,7 +6,7 @@ List_master = {int(line.split()[0]): [int(val) for val in line.split()[1:] if va
 def main():
   min_cut = 10000000000000000000
 #  G = {1: [2,3,3], 2:[1,3], 3:[1,1,2]}
-  for i in range(1, 1000000):
+  for i in range(1, 200*200):
     G = List_master
     while len(G.keys()) > 2:
       n1 = rand.choice(G.keys())
@@ -22,7 +22,8 @@ def main():
       # Remove self loops
       G[n1] = [edge for edge in G[n1] if edge != n1]
       cut = len(G[G.keys()[0]])
-    if  min_cut and cut < min_cut:
+    if min_cut and cut < min_cut:
+      print min_cut
       min_cut = cut  
   print min_cut
   
