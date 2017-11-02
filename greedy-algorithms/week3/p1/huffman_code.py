5478a57a9f7c27aa8574c1406c44c98aa20491e4
John <johnwilles@gmail.com>
2017-11-01 20:49:59 -0400

    Add heap tuple implementatino

--- greedy-algorithms/week3/p1/huffman_code.py
+++ greedy-algorithms/week3/p1/huffman_code.py
@@ -1,17 +1,28 @@
+import heapq
+
 def process_text(filename):
   f = open(filename, 'r').readlines()
   num_symbols = int(f[0][:-1])
-  symbols = {ind: int(x[:-1]) for ind, x in enumerate(f[1:])}
-#  symbols = dict(f[1:])
-  return num_symbols, symbols
+  symbols = [(int(weight[:-1]), ind) for ind, weight in enumerate(f[1:])]
+  symbols_dict = {ind: int(x[:-1]) for ind, x in enumerate(f[1:])}
+#  symbols = {int(x[:-1]): ind  for ind, x in enumerate(f[1:])}
+  return num_symbols, symbols, symbols_dict
+
+
 
 def main():
 
   filename = 'huffman.txt'
-  num_symbols, symbols = process_text(filename)
+  num_symbols, symbols, symbols_dict = process_text(filename)
 
-#  print symbols
-  print num_symbols
+  heapq.heapify(symbols)
+
+#  print heapq.heappop(symbols)
+#  print heapq.heappop(symbols)
+
+  print symbols
+#  print num_symbols
+#  print weights
 
 if __name__ == '__main__':
   main()
