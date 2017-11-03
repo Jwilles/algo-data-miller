0febe6dff7a8afe2ac21535a36384f860ca8d511
John <johnwilles@gmail.com>
2017-11-03 00:54:38 -0400

    Finish week3 p1 and p2

--- greedy-algorithms/week3/p1/huffman_code.py
+++ greedy-algorithms/week3/p1/huffman_code.py
@@ -3,7 +3,7 @@ import heapq
 def process_text(filename):
   f = open(filename, 'r').readlines()
   num_symbols = int(f[0][:-1])
-  symbols = [(int(weight[:-1]), ind) for ind, weight in enumerate(f[1:])]
+  symbols = [(int(weight[:-1]), [ind]) for ind, weight in enumerate(f[1:])]
   symbols_dict = {ind: int(x[:-1]) for ind, x in enumerate(f[1:])}
 #  symbols = {int(x[:-1]): ind  for ind, x in enumerate(f[1:])}
   return num_symbols, symbols, symbols_dict
@@ -23,23 +23,24 @@ def merge(symbols):
 
 
 def max_depth(tree):
-  depth = 1
-  print tree
-  while type(tree) is not int:
-   #print tree
-   depth += 1
-   tree = tree[1]
-  depth += 1
-  return depth 
+  if len(tree) == 1:
+    return 0
+  else:
+    return 1 + max(max_depth(tree[0]), max_depth(tree[1]))
 
+def min_depth(tree):
+  if len(tree) == 1:
+    return 0
+  else:
+    return 1 + min(min_depth(tree[0]), min_depth(tree[1]))
 
 def main():
 
   filename = 'huffman.txt'
   num_symbols, symbols, symbols_dict = process_text(filename)
 
-  print num_symbols
-  print symbols_dict
+#  print num_symbols
+#  print symbols_dict
 
   heapq.heapify(symbols)
 
@@ -50,9 +51,11 @@ def main():
     symbols = merge(symbols)
 
   max_code_length = max_depth(symbols[0][1])
+  min_code_length = min_depth(symbols[0][1])
 
 #  print symbols[0][1][1]
   print max_code_length
+  print min_code_length
 
 #  print num_symbols
 #  print weights
