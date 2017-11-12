b44b1a43eae9d3edbf209ce9f79fdbd2f9ebfc3b
John <johnwilles@gmail.com>
2017-11-12 16:56:19 -0500

    p2 space optimization

--- greedy-algorithms/week4/p2/knapsack_large.py
+++ greedy-algorithms/week4/p2/knapsack_large.py
@@ -1,4 +1,10 @@
-import heapq
+#import heapq
+
+#import sys
+
+#sys.setrecursionlimit(10000)
+
+
 
 def process_text(filename):
   f = open(filename, 'r').readlines()
@@ -8,19 +14,33 @@ def process_text(filename):
   return size, num_items, items 
 
 
-
 def optimal_knapsack(size, num_items, items):
 
-  A = [[0 for i in range(0, size)] for j in range(0,num_items)]
+  A = [[0 for i in range(0, size)] for j in range(0,2)]
 
   for i in range(0, num_items):
+    
+    print i
+    A[0] = A[1]
+
     for j in range(0, size):
       if items[i][1] > j:
-        A[i][j] = A[i-1][j]
+        A[1][j] = A[0][j]
       else:
-        A[i][j] = max(A[i-1][j], A[i-1][j-items[i][1]] + items[i][0])
+        A[1][j] = max(A[0][j], A[0][j-items[i][1]] + items[i][0])
 
-  return A[num_items-1][size-1] 
+  return A[1][size-1] 
+
+def recursive_knapsack(size, num_items, items):
+
+  if len(items) == 1:
+    return items[0][0]
+  else:
+    next_item = items[0]
+    next_items = items[1:]
+    next_num_items = num_items - 1 
+
+    return max(recursive_knapsack(size, num_items, items), recursive_knapsack((size - next_item[1]), next_num_items, next_items) + next_item[0])
 
 
 def main():
@@ -29,6 +49,7 @@ def main():
   size, num_items, items  = process_text(filename)
 
   max_val = optimal_knapsack(size, num_items, items)
+#  max_val = recursive_knapsack(size, num_items, items)
 
 #  print size  
 #  print num_items 
