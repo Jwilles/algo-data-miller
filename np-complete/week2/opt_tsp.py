bb49bc4be09bae40b09707b047109d6f74a8fe0a
John <johnwilles@gmail.com>
2017-12-14 20:46:44 -0500

    Parse tsp.txt

--- np-complete/week2/opt_tsp.py
+++ np-complete/week2/opt_tsp.py
@@ -1,4 +1,22 @@
 
+def process_text(filename):
+  
+  f = open(filename, 'r').readlines()
+  graph_info = int(f[0][:-1])
+  points = [map(float,line[:-1].split(" ")) for line in f[1:]]
+
+  return graph_info, points
+
+
+def main():
+
+  filename = 'tsp.txt'
+  G_info, points = process_text(filename)
+ 
+  print G_info
+  print points 
+
+
 
 if __name__ == "__main__":
-  print 'hello' 
+  main()
